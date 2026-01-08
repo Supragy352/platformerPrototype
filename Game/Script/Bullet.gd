3acf123dfe27e2077137c637f3778dd8ab3694b5
Supragy Mishra <mishra03supragya@gmail.com>
2026-01-08 17:58:09 +0530

    first commit

--- Game/Script/Bullet.gd
+++ Game/Script/Bullet.gd
@@ -6,22 +6,20 @@ const DAMAGE = 35
 @onready var bullet_sprite_2d = $Bullet_Sprite2D
 
 func _physics_process(delta):
-	if direction == -1:					
+	if direction == -1:
 		bullet_sprite_2d.flip_h = true
-		
-	position.x += SPEED * direction * delta
 
+	position.x += SPEED * direction * delta
 
 func _on_body_entered(body):
 	var vfxToSpawn = preload("res://Game/Scene/vfx_bullet_hit.tscn")
 	var vfxInstance = GameManager.SpawnVFX(vfxToSpawn, global_position)
-	
+
 	if direction == -1:
 		vfxInstance.scale.x = -1
-	
+
 	var enemy = body as EnemyController
 	if enemy:
 		enemy.ApplyDamage(DAMAGE)
-	
+
 	queue_free()
- 
